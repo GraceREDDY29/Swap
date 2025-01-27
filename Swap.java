a9d303ba267056f42ce538856a41cbc5994eafc4
GraceREDDY29 <gundampalligrace@gmail.com>
2025-01-27 23:08:00 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,12 @@
+public class Swap {
+    
+        public static void main(String[] args) {
+            int A = 8, B = 20, temp;
+            temp = A;
+            A = B;
+            B = temp;
+            System.out.printf("A is %d, B is %d",A, B);
+        }
+    }
+
+    
